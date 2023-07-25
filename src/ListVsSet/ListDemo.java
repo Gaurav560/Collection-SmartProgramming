2ae6048b14df1df8da60c89c7b88cb55c596c000
GAURAV SHARMA <gsharma1154@gmail.com>
2023-07-25 11:40:24 +0530

    list iterator is not present in set interface but present in list interface

--- src/ListVsSet/ListDemo.java
+++ src/ListVsSet/ListDemo.java
@@ -3,6 +3,7 @@ package ListVsSet;
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
+import java.util.ListIterator;
 
 public class ListDemo {
     public static void main(String[] args) {
@@ -20,11 +21,15 @@ public class ListDemo {
 
         //list interface ka ek method hota hai iterator  to iterate over an list and get its element
         //list.iterator return object of iterator interface
-       Iterator itr= l1.iterator();
+     //  Iterator itr= l1.iterator();
        //next method of iterator class return the next object as it starts before  0 th index in list
-        while (itr.hasNext())
-        System.out.println(itr.next());
+       // while (itr.hasNext())
+       // System.out.println(itr.next());
 
+        ListIterator lI1=l1.listIterator();
+        while (lI1.hasNext()){
+            System.out.println(lI1.next());
+        }
 
     }
 }
