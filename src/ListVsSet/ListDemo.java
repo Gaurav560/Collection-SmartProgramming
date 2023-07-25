9e232b63a7ab209f747528b0adab9150a2c165bf
GAURAV SHARMA <gsharma1154@gmail.com>
2023-07-25 11:23:58 +0530

    list follows insertion order ,stores null values, allows duplicacy, index based insertion  where as hashset does not do this(but can contain only one null values)

--- src/ListVsSet/ListDemo.java
+++ src/ListVsSet/ListDemo.java
@@ -1,7 +1,22 @@
 package ListVsSet;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class ListDemo {
     public static void main(String[] args) {
-        
+
+        List l1=new ArrayList();
+        l1.add(544);
+        l1.add(524);
+        l1.add(544);
+        l1.add(544);
+        l1.add(null);
+        l1.add(null);
+        l1.add(null);
+        System.out.println(l1);
+
+
+
     }
 }
--- src/ListVsSet/SetDemo.java
+++ src/ListVsSet/SetDemo.java
@@ -1,7 +1,22 @@
 package ListVsSet;
 
+import java.util.HashSet;
+import java.util.Set;
+
 public class SetDemo {
     public static void main(String[] args) {
+
+        Set s1= new HashSet();
+        s1.add(44);
+        s1.add(434);
+        s1.add(42);
+        s1.add(24);
+        s1.add(24);
+        s1.add(null);
+        System.out.println(s1);
+
+
+
 
     }
 }
