2dcfac685bda88f09797fec5f610de70c8b2d0c2
GAURAV SHARMA <gsharma1154@gmail.com>
2023-07-25 11:35:01 +0530

    list iterator is not present in set interface but present in list interface

--- src/ListVsSet/SetDemo.java
+++ src/ListVsSet/SetDemo.java
@@ -1,6 +1,7 @@
 package ListVsSet;
 
 import java.util.HashSet;
+import java.util.Iterator;
 import java.util.Set;
 
 public class SetDemo {
@@ -14,7 +15,12 @@ public class SetDemo {
         s1.add(24);
         s1.add(null);
         System.out.println(s1);
+Iterator itr2=s1.iterator();
 
+//in case of set interface ,it has only itertator method but not listIterator method
+        while(itr2.hasNext()){
+            System.out.println(itr2.next());
+        }
 
 
 
